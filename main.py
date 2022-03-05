59ec5e28a722f940c80a74d97b3b736c72846504
jkroeker98 <josephkroeker@gmail.com>
2022-03-05 10:33:30 -0700

    Made generic bot class

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 # main.py
 import os
 
-import discord
+import discord as ds
 from dotenv import load_dotenv
 
 load_dotenv()
-
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = ds.Intents.all()
+client = ds.Client(intents=intents)
 
-@client.event
 
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+class GenericClient(ds.Client):
+    async def on_ready(self): # Notify when connected to server
+        print(f'{client.user} has connected to Discord!\n')
 
-    client.run(TOKEN)
-    
+    async def on_message(self, message):
+        if message.author == client.user: # No reason to read my own messages
+            return
+
+        if message.content == '!helpme' or message.content == '!h':
+            response = self.help_msg(self)
+
+        await message.channel.send(response)
+
+    def help_msg(self):
+        return "There is no defined help for this bot at the moment"
+
+
+client = GenericClient()
+client.run(TOKEN)
